2fed4b021d31be79b5c2794b99f499e65b3e0dd1
assuerolopo <assuerolopo@gmail.com>
2023-02-27 11:16:00 -0300

    Codigo_V01.R: Dado truncado em 1 casa decimal. "M" adicionado ao Tooltip. "Obs:.." adicionado ao gráfico.

--- codigo_V01.R
+++ codigo_V01.R
@@ -53,7 +53,7 @@ dados <- read_excel("data/t128.xlsx")
 names(dados)
 dados %<>% mutate(dados, `Industrial` = `Industrial`/1000)
 # Temas Subtemas Perguntas
-
+dados <- dados %>% mutate(across(`Industrial`, ~round(.x,1)))
 ##  Perguntas e titulos 
 T_ST_P_No_MEIOAMBIENTE <- read_csv("data/TEMA_SUBTEMA_P_No - MEIOAMBIENTE.csv")
 
@@ -96,13 +96,14 @@ data_serie <- paste('[',gsub(' ',',',
 texto<-paste('{"title":{"text":"',titulo,
              '","subtext":"',subtexto,
              '","sublink":"',link,'"},',
-             '"tooltip":{"trigger":"axis"},',
+             '"tooltip":{"trigger":"item","responsive":"true","position":"top","formatter":"{c0} M"},',
              '"toolbox":{"left":"center","orient":"horizontal","itemSize":20,"top":20,"show":true,',
              '"feature":{"dataZoom":{"yAxisIndex":"none"},',
              '"dataView":{"readOnly":false},"magicType":{"type":["line","bar"]},',
              '"restore":{},"saveAsImage":{}}},"xAxis":{"type":"category",',
              '"data":',data_axis,'},',
              '"yAxis":{"type":"value","axisLabel":{"formatter":"{value} M"}},',
+             '"graphic":[{"type":"text", "left":"center","top":"bottom","z":100, "style":{"fill":"gray","text":"Obs: Ponto é separador decimal", "font":"8px sans-srif","fontSize":12}}],',
              '"series":[{"data":',data_serie,',',
              '"type":"bar","color":"',corsec_recossa_azul[2],'","showBackground":true,',
              '"backgroundStyle":{"color":"rgba(180, 180, 180, 0.2)"},',
